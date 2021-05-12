c1eb014b9c146412d6c3c20347f9eeb846c58dd8
Manuel Garcia <kiramanuel123@gmail.com>
2021-05-12 01:04:38 -0500

    Agregar archivos de proyecto.

---
+++ Java-Stdin-and-StdOut-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Java_Stdin_and_StdOut_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introudce una cadena y un numero sin punto decimal");
+            String myString = Console.ReadLine();
+            String entrada = Console.ReadLine();
+            int myInt = Convert.ToInt32(entrada);
+            Console.WriteLine("My string: " + myString);
+            Console.WriteLine("My int: " + entrada);
+        }
+    }
+}
